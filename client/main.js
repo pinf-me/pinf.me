6ce04ba4776ff5244c7f4f96b7a7c39d68252c27
Christoph Dorn <christoph@christophdorn.com>
2015-05-05 22:00:40 -0700

    wip

---
+++ client/main.js
@@ -0,0 +1,13 @@
+
+exports.main = function (API) {
+
+	var exports = {};
+
+	exports.turn = function (resolvedConfig) {
+
+		API.insight.debug("CLIENT MAIN for PINF.me!");
+
+	}
+
+	return exports;
+}
